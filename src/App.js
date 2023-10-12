ebb500762df1d97de77c24b77a42d32951275b73
SlimTracy <90283597+SlimTracy@users.noreply.github.com>
2023-10-11 23:29:17 -0500

    used https on api call

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=57c4b163";
+const API_URL = "https://www.omdbapi.com?apikey=57c4b163";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
